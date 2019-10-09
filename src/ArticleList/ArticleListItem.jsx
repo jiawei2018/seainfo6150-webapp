4763dffc27c85257ddd485d00f8f382dbfd4bcfd
jiawei2018 <35362950+jiawei2018@users.noreply.github.com>
2019-10-09 16:08:15 -0700

    Update ArticleListItem.jsx

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -10,7 +10,7 @@ class ArticleListItem extends React.Component {
             <div>
                 <h2>{this.props.article.title}</h2>
                 <p>{this.props.article.shortText}</p>
-                <time dateTime="2019-09-22">{this.props.article.pubDate}</time>
+                <time dateTime={this.props.article.pubDate}>{this.props.article.pubDate}</time>
                 <button onClick={() => {javaScript:alert(this.props.article.slug)}}>
                     show article slug
                 </button>

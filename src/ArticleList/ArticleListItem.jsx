30b977beb9ad7b98e41d2dde8386ddf55403d222
jiawei2018 <35362950+jiawei2018@users.noreply.github.com>
2019-10-09 16:05:35 -0700

    Update ArticleListItem.jsx

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -12,7 +12,7 @@ class ArticleListItem extends React.Component {
             <div className= {box.outBox}>
                 <h2 className= {box.h2Style}>{this.props.article.title}</h2>
                 <p className={box.title}>{this.props.article.shortText}</p>
-                <time className= {box.timeStyle}>{this.props.article.pubDate}</time>
+                <time className= {box.timeStyle} dateTime="2019-09-22">{this.props.article.pubDate}</time>
                 <button className= {bton.authorButton} onClick={() => {javaScript:alert(this.props.article.slug)}}>
                     {this.props.article.author}
                 </button>
